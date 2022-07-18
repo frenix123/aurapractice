43e4f83fce5b8a50c1905a7d1bf595446491ab98
Frenix <frenix@gmail.com>
2022-07-19 03:35:16 +0530

    Good commit

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,7 +1,12 @@
 ({
     doThis : function(component, event, helper) {
         console.log("Initialization Complete");
-        console.log(helper.helperMethod(8));
+        const words = helper.helperMethod(8);
+        console.log(words);
+        const winnerWord = helper.winWord(words)
+        console.log(winnerWord);
+        component.set("v.words",words);
+        component.set("v.winWord", winnerWord);
 
     }
 })
