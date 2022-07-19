8743f47160f42da47445797d22e2fe8b2609ade1
Frenix <frenix@gmail.com>
2022-07-20 02:56:31 +0530

    Awesome Learning

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,12 +1,25 @@
 ({
     doThis : function(component, event, helper) {
         console.log("Initialization Complete");
-        const words = helper.helperMethod(8);
+        let hardness = 0;
+        const mode = component.get("v.mode");
+        if(mode && mode ==="Easy"){
+            hardness=3;
+        }else if(mode==="Medium"){
+            hardness=4;
+        }else{
+            hardness=6
+        }
+        const words = helper.helperMethod(hardness*hardness);
         console.log(words);
         const winnerWord = helper.winWord(words)
         console.log(winnerWord);
         component.set("v.words",words);
         component.set("v.winWord", winnerWord);
+
+        const blocksize = 12/hardness;
+        component.set("v.blocksize",blocksize);
+        
 
     }
 })
---
+++ force-app/main/default/aura/Tiles/TilesController.js
@@ -0,0 +1,9 @@
+({
+    Choose : function(component, event, helper) {
+        const open = component.get("v.open");
+        if(!open){
+            component.set("v.open",true);
+        }
+
+    }
+})
